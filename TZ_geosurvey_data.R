079a1dc09669f6fc5f1d683e80eb8458aa55c871
mgwalsh <markusgwalsh@gmail.com>
2014-11-10 05:36:44 +0300

    Documentation update

--- TZ_geosurvey_data.R
+++ TZ_geosurvey_data.R
@@ -12,8 +12,7 @@ require(proj4)
 require(raster)
 
 # Data downloads ----------------------------------------------------------
-
-# Geosurvey data
+# GeoSurvey data
 download("https://www.dropbox.com/s/8dgzphvwn0ls4sq/TZ_geos_0914.csv?dl=0", "TZ_geos_0914.csv", mode="wb")
 geos <- read.table("TZ_geos_0914.csv", header=T, sep=",")
 
@@ -22,7 +21,6 @@ download("https://www.dropbox.com/s/0ucx0oqx8c4lpej/TZ_grids.zip?dl=0", "TZ_grid
 unzip("TZ_grids.zip", overwrite=T)
 
 # LAEA CRS & grid ID's ----------------------------------------------------
-
 # Project to Africa LAEA from LonLat
 geos.laea <- as.data.frame(project(cbind(geos$Lon, geos$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
 colnames(geos.laea) <- c("x","y")
@@ -40,9 +38,7 @@ coordinates(geos.gid) = ~x+y
 proj4string(geos.gid) = CRS("+proj=laea +datum=WGS84 +ellps=WGS84 +lat_0=5 +lon_0=20 +no_defs")
 
 # Grid overlay ------------------------------------------------------------
-
 grid.list <- c("BSANs.tif","BSASs.tif","BSAVs.tif","CTIs.tif","ELEVs.tif","EVIAs.tif","LSTDs.tif","LSTNs.tif","REF1s.tif","REF2s.tif","REF3s.tif","REF7s.tif","RELIs.tif","TMAPs.tif","TMFIs.tif")
-
 for (i in 1:length(grid.list)){
   print(paste("extracting", grid.list[i]))
   grid.cov <- raster(grid.list[i]) 
@@ -54,7 +50,6 @@ for (i in 1:length(grid.list)){
 TZ_GS_data <- as.data.frame(geos.gid)
 
 # Write csv ---------------------------------------------------------------
-
 write.csv(TZ_GS_data, "TZ_geosurvey.csv")
 
 
