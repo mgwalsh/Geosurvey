c00a1807129dd85af8dd90b10ed01a2d23d3f107
mgwalsh <markusgwalsh@gmail.com>
2014-11-17 11:36:28 +0300

    Documentation update

--- Geosurvey_sample.R
+++ Geosurvey_sample.R
@@ -1,10 +1,10 @@
 # Generate GeoSurvey sampling points in Africa on a 1 km grid
-# J. Chen, W. Wu & M. Walsh, November, 2014
+# J. Chen, W. Wu & M. Walsh, November 2014
 
 require(downloader)
 require(rgdal)
 
-# Download sampling mask grid ---------------------------------------------
+# Download sampling mask grid (fAPAR > 0) ---------------------------------
 download("https://www.dropbox.com/s/brikq1ceek16w3s/Af_mask.tif.zip?dl=0", "AF_mask.tif.zip", mode="wb")
 unzip("Af_mask.tif.zip", overwrite=T)
 mask <- readGDAL("AF_mask.tif", silent=TRUE)
