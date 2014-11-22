2752c436c090a6176bd3fbf7cd5cad506e7ac67e
mgwalsh <markusgwalsh@gmail.com>
2014-11-22 08:54:34 +0300

    stepwise GLM predictions

--- TZ_GS_ensemble.R
+++ TZ_GS_ensemble.R
@@ -18,8 +18,9 @@ dir.create("Data", showWarnings=F)
 dat_dir <- "./Data"
 
 # download GeoSurvey data
-download("https://www.dropbox.com/s/8dgzphvwn0ls4sq/TZ_geos_0914.csv?dl=0", "./Data/TZ_geos_0914.csv", mode="wb")
-geos <- read.table(paste(dat_dir, "/TZ_geos_0914.csv", sep=""), header=T, sep=",")
+download("https://www.dropbox.com/s/03l4m4zjdi5mhyu/TZ_geos_1114.csv?dl=0", "./Data/TZ_geos_1114.csv", mode="wb")
+geos <- read.table(paste(dat_dir, "/TZ_geos_1114.csv", sep=""), header=T, sep=",")
+geos <- na.omit(geos)
 
 # download Tanzania Gtifs (~27.2 Mb) and stack in raster
 download("https://www.dropbox.com/s/otiqe78s0kf1z1s/TZ_grids.zip?dl=0", "./Data/TZ_grids.zip", mode="wb")
@@ -39,36 +40,66 @@ projection(geos) <- projection(grid)
 geosgrid <- extract(grid, geos)
 
 # Assemble dataframes
-# presence/absence of Cropland (CRP, present = 1, absent = 0)
+# presence/absence of Cropland (CRP, present = P, absent = A)
 CRP <- geos$CRP
-crpdat <- data.frame(cbind(CRP, geosgrid))
+crpdat <- cbind.data.frame(CRP, geosgrid)
 crpdat <- na.omit(crpdat)
 
-# presence/absence of Woody Vegetation Cover of >60% (WCP, present = 1, absent = 0)
+# presence/absence of Woody Vegetation Cover of >60% (WCP, present = P, absent = A)
 WCP <- geos$WCP
-wcpdat <- data.frame(cbind(WCP, geosgrid))
+wcpdat <- cbind.data.frame(WCP, geosgrid)
 wcpdat <- na.omit(wcpdat)
 
-# presence/absence of Buildings (HSP, present = 1, absent = 0)
+# presence/absence of Buildings/Human Settlements (HSP, present = P, absent = A)
 HSP <- geos$HSP
-hspdat <- data.frame(cbind(HSP, geosgrid))
+hspdat <- cbind.data.frame(HSP, geosgrid)
 hspdat <- na.omit(hspdat)
 
 # Split data into train and test sets ------------------------------------
 set.seed(1385321)
 
-# Cropland data split
+# Cropland train/test split
 crpIndex <- createDataPartition(crpdat$CRP, p = 0.75, list = FALSE, times = 1)
 crpTrain <- crpdat[ crpIndex,]
 crpTest  <- crpdat[-crpIndex,]
 
-# Woody cover data split
+# Woody cover train/test split
 wcpIndex <- createDataPartition(wcpdat$WCP, p = 0.75, list = FALSE, times = 1)
 wcpTrain <- wcpdat[ wcpIndex,]
 wcpTest  <- wcpdat[-wcpIndex,]
 
-# Settlement data split
+# Settlement train/test split
 hspIndex <- createDataPartition(hspdat$HSP, p = 0.75, list = FALSE, times = 1)
 hspTrain <- hspdat[ hspIndex,]
 hspTest  <- hspdat[-hspIndex,]
+
+# Cross-validation setup --------------------------------------------------
+# 10-fold
+cv10 <- trainControl(method = "cv", number = 10)
+
+# Classification models ---------------------------------------------------
+# Stepwise main effects GLM's
+CRP.glm <- train(CRP ~ ., data = crpTrain,
+                 family = binomial, 
+                 method = "glmStepAIC",
+                 trControl = cv10)
+crpglm.test <- predict(CRP.glm, crpTest, type = "prob")
+crpglm.pred <- predict(grid, CRP.glm, type = "prob")
+plot(1-crpglm.pred)
+
+WCP.glm <- train(WCP ~ ., data = wcpTrain,
+                 family=binomial, 
+                 method = "glmStepAIC",
+                 trControl = cv10)
+wcpglm.test <- predict(WCP.glm, wcpTest, type = "prob")
+wcpglm.pred <- predict(grid, WCP.glm, type = "prob")
+plot(1-wcpglm.pred)
+
+HSP.glm <- train(HSP ~ ., data = hspTrain,
+                 family=binomial, 
+                 method = "glmStepAIC",
+                 trControl = cv10)
+hspglm.test <- predict(HSP.glm, hspTest, type = "prob")
+hspglm.pred <- predict(grid, HSP.glm, type = "prob")
+plot(1-hspglm.pred)
 
