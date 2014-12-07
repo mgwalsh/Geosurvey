6205cae107275269bbda39adeff5bd5e5202a20a
mgwalsh <markusgwalsh@gmail.com>
2014-12-07 04:03:10 +0300

    added main predictions

--- ET_GW_ensemble.R
+++ ET_GW_ensemble.R
@@ -69,3 +69,118 @@ hspIndex <- createDataPartition(hspdat$HSP, p = 0.75, list = FALSE, times = 1)
 hspTrain <- hspdat[ hspIndex,]
 hspTest  <- hspdat[-hspIndex,]
 
+# Stepwise main effects GLM's <MASS> --------------------------------------
+# 10-fold CV
+step <- trainControl(method = "cv", number = 10)
+
+# presence/absence of Cropland (CRP, present = Y, absent = N)
+CRP.glm <- train(CRP ~ ., data = crpTrain,
+                 family = binomial, 
+                 method = "glmStepAIC",
+                 trControl = step)
+crpglm.test <- predict(CRP.glm, crpTest) ## predict test-set
+confusionMatrix(crpglm.test, crpTest$CRP, "Y") ## print validation summaries
+crpglm.pred <- predict(grid, CRP.glm, type = "prob") ## spatial predictions
+
+# presence/absence of Human Settlements (HSP, present = Y, absent = N)
+HSP.glm <- train(HSP ~ ., data = hspTrain,
+                 family=binomial, 
+                 method = "glmStepAIC",
+                 trControl = step)
+hspglm.test <- predict(HSP.glm, hspTest) ## predict test-set
+confusionMatrix(hspglm.test, hspTest$HSP, "Y") ## print validation summaries
+hspglm.pred <- predict(grid, HSP.glm, type = "prob") ## spatial predictions
+
+# Plot <MASS> predictions
+glmpreds <- stack(1-crpglm.pred, 1-hspglm.pred)
+names(glmpreds) <- c("CRPglm", "HSPglm")
+plot(glmpreds, axes = F)
+
+# Random forests <randomForest> -------------------------------------------
+# out-of-bag predictions
+oob <- trainControl(method = "oob")
+
+# presence/absence of Cropland (CRP, present = Y, absent = N)
+CRP.rf <- train(CRP ~ ., data = crpTrain,
+                method = "rf",
+                trControl = oob)
+crprf.test <- predict(CRP.rf, crpTest) ## predict test-set
+confusionMatrix(crprf.test, crpTest$CRP, "Y") ## print validation summaries
+crprf.pred <- predict(grid, CRP.rf, type = "prob") ## spatial predictions
+
+# presence/absence of Human Settlements (HSP, present = Y, absent = N)
+HSP.rf <- train(HSP ~ ., data = hspTrain,
+                method = "rf",
+                trControl = oob)
+hsprf.test <- predict(HSP.rf, hspTest) ## predict test-set
+confusionMatrix(hsprf.test, hspTest$HSP, "Y") ## print validation summaries
+hsprf.pred <- predict(grid, HSP.rf, type = "prob") ## spatial predictions
+
+# Plot <randomForest> predictions
+rfpreds <- stack(1-crprf.pred, 1-hsprf.pred)
+names(rfpreds) <- c("CRPrf", "HSPrf")
+plot(rfpreds, axes = F)
+
+# Gradient boosting <gbm> ------------------------------------------
+# CV for training gbm's
+gbm <- trainControl(method = "repeatedcv", number = 10, repeats = 5)
+
+# presence/absence of Cropland (CRP, present = Y, absent = N)
+CRP.gbm <- train(CRP ~ ., data = crpTrain,
+                 method = "gbm",
+                 trControl = gbm)
+crpgbm.test <- predict(CRP.gbm, crpTest) ## predict test-set
+confusionMatrix(crpgbm.test, crpTest$CRP, "Y") ## print validation summaries
+crpgbm.pred <- predict(grid, CRP.gbm, type = "prob") ## spatial predictions
+
+# presence/absence of Human Settlements (HSP, present = Y, absent = N)
+HSP.gbm <- train(HSP ~ ., data = hspTrain,
+                 method = "gbm",
+                 trControl = gbm)
+hspgbm.test <- predict(HSP.gbm, hspTest) ## predict test-set
+confusionMatrix(hspgbm.test, hspTest$HSP, "Y") ## print validation summaries
+hspgbm.pred <- predict(grid, HSP.gbm, type = "prob") ## spatial predictions
+
+# Plot <gbm> predictions
+gbmpreds <- stack(1-crpgbm.pred, 1-hspgbm.pred)
+names(gbmpreds) <- c("CRPgbm", "HSPgbm")
+plot(gbmpreds, axes = F)
+
+# Neural nets <nnet> ------------------------------------------------------
+# CV for training nnet's
+nn <- trainControl(method = "cv", number = 10)
+
+# presence/absence of Cropland (CRP, present = Y, absent = N)
+CRP.nn <- train(CRP ~ ., data = crpTrain,
+                method = "nnet",
+                trControl = nn)
+crpnn.test <- predict(CRP.nn, crpTest) ## predict test-set
+confusionMatrix(crpnn.test, crpTest$CRP, "Y") ## print validation summaries
+crpnn.pred <- predict(grid, CRP.nn, type = "prob") ## spatial predictions
+
+# presence/absence of Human Settlements (HSP, present = Y, absent = N)
+HSP.nn <- train(HSP ~ ., data = hspTrain,
+                method = "nnet",
+                trControl = nn)
+hspnn.test <- predict(HSP.nn, hspTest) ## predict test-set
+confusionMatrix(hspnn.test, hspTest$HSP, "Y") ## print validation summaries
+hspnn.pred <- predict(grid, HSP.nn, type = "prob") ## spatial predictions
+
+# Plot <nnet> predictions
+nnpreds <- stack(1-crpnn.pred, 1-hspnn.pred)
+names(nnpreds) <- c("CRPnn", "HSPnn")
+plot(nnpreds, axes = F)
+
+# Plot predictions by Geo-Wiki variables ---------------------------------
+# Cropland prediction plots
+crp.preds <- stack(1-crpglm.pred, 1-crprf.pred, 1-crpgbm.pred, 1-crpnn.pred)
+names(crp.preds) <- c("glm","randomForest","gbm","nnet")
+plot(crp.preds, axes = F)
+
+# Human settlement prediction plots
+hsp.preds <- stack(1-hspglm.pred, 1-hsprf.pred, 1-hspgbm.pred, 1-hspnn.pred)
+names(hsp.preds) <- c("glm","randomForest","gbm","nnet")
+plot(hsp.preds, axes = F)
+
+
+
