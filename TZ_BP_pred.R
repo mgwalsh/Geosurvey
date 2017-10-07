69c6340946975895c626aea11a6be119e3630a4f
mgwalsh <markusgwalsh@gmail.com>
2017-10-07 06:51:45 +0300

    Stacked predictions of Tanzania presence of buildings observations

--- TZ_BP_pred.R
+++ TZ_BP_pred.R
@@ -126,7 +126,7 @@ registerDoParallel(mc)
 
 # control setup
 set.seed(1385321)
-tc <- trainControl(method = "repeatedcv", number=5, classProbs = TRUE,
+tc <- trainControl(method = "repeatedcv", repeats=5, classProbs = TRUE,
                    summaryFunction = twoClassSummary, allowParallel = T)
 
 # model training
@@ -198,5 +198,4 @@ plot(cp_eval, 'ROC') ## plot ROC curve
 # Write prediction files --------------------------------------------------
 bppreds <- stack(preds, 1-bpst.pred)
 names(bppreds) <- c("bprf","bpgb","bpnn","bprr","bpst")
-dir.create("Results", showWarnings=F)
 writeRaster(bppreds, filename="./Results/TZ_bppreds_2017.tif", datatype="FLT4S", options="INTERLEAVE=BAND", overwrite=T)
