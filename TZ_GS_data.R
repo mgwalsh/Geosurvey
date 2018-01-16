859a6aa668540a31dd378706c46e2b4522c3827b
mgwalsh <markusgwalsh@gmail.com>
2018-01-16 04:04:26 +0300

    Tanzania GeoSurvey 250m resolution data setup

--- TZ_GS_data.R
+++ TZ_GS_data.R
@@ -21,6 +21,7 @@ setwd("./TZ_GS250")
 download("https://www.dropbox.com/s/57kuxbkm5sv092a/TZ_geos_2017.csv.zip?raw=1", "TZ_geos_2017.csv.zip", mode = "wb")
 unzip("TZ_geos_2017.csv.zip", overwrite = T)
 geos <- read.table("TZ_geos_2017.csv", header = T, sep = ",")
+geos$PC <- as.factor(ifelse(geos$CP == "Y" & geos$BP == "Y", "Y", "N")) ## identifies croplands with buildings
 
 # download GADM-L3 shapefile (courtesy: http://www.gadm.org)
 download("https://www.dropbox.com/s/bhefsc8u120uqwp/TZA_adm3.zip?raw=1", "TZA_adm3.zip", mode = "wb")
@@ -40,7 +41,7 @@ projection(geos) <- projection(shape)
 gadm <- geos %over% shape
 geos <- as.data.frame(geos)
 geos <- cbind(gadm[ ,c(5,7,9)], geos)
-colnames(geos) <- c("Region", "District", "Ward", "Observer", "lat", "lon", "BP", "CP", "WP")
+colnames(geos) <- c("Region", "District", "Ward", "Observer", "lat", "lon", "BP", "CP", "WP", "PC")
 
 # project GeoSurvey coords to grid CRS
 geos.proj <- as.data.frame(project(cbind(geos$lon, geos$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
