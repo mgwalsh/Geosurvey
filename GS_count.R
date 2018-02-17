9f297c2fda42a5227adf3a1f1d7a7315d998bf3b
mgwalsh <markusgwalsh@gmail.com>
2018-02-17 07:10:54 +0300

    GeoSurvey building count data setup

---
+++ GS_count.R
@@ -0,0 +1,34 @@
+# GeoSurvey building count data setup 
+# M. Walsh, February 2018
+
+# Required packages
+# install.packages(c("downloader","rgdal","jsonlite")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(jsonlite)
+})
+
+# Data downloads -----------------------------------------------------------
+# set working directory
+dir.create("test", showWarnings=F)
+setwd("./test")
+
+# download GeoSurvey data
+download("https://www.dropbox.com/s/qyz01a6dbtae06p/export-malawi-buildings.csv.zip?raw=1", "export-malawi-buildings.csv.zip", mode="wb")
+unzip("export-malawi-buildings.csv.zip", overwrite=T)
+geos <-  read.csv("export-malawi-buildings.csv", stringsAsFactors = F)
+is.na(geos$building_loc) <- geos$building == "No"
+bp <- geos[ which(geos$building == "Yes"), ]
+
+# Count number of buildings per quadrat -----------------------------------
+n <- rep(NA, nrow(bp))
+for(i in 1:nrow(bp)) {
+  t <- fromJSON(bp$building_loc[i])
+  n[i] <- nrow(t$features)
+}
+n
+
+# Write file --------------------------------------------------------------
+bp <- cbind(bp, n)
+write.csv(bp, "building_count.csv", row.names = F)
